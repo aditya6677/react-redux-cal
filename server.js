8887367befc1323d697db1a22e6c1a3adfea3a6f
Ubuntu <ubuntu@ip-172-31-39-37.us-east-2.compute.internal>
2018-02-24 21:14:34 +0000

    final version with lgake tharmameter

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'http://letscode.live');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
